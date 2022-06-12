bf10e4a3b8a3187135ef5f41adc1a40ebbd9823c
Aniketh961 <221910313003@gitam.in>
2022-06-12 16:00:54 +0530

    highlight winning squares and add start game button

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
 
+const NEW_GAME = [{ board: Array(9).fill(null), isXNext: true }];
+
 const app = () => {
-  const [history, setHistory] = useState([
-    { board: Array(9).fill(null), isXNext: true },
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
   const [currentMove, setCurrentMove] = useState(0);
 
   const current = history[currentMove];
 
-  const winner = calculateWinner(current.board);
+  const { winner, winningSquares } = calculateWinner(current.board);
 
   const handelSquareClick = position => {
     if (current.board[position] || winner) {
@@ -42,11 +42,23 @@ const app = () => {
     setCurrentMove(move);
   };
 
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <StatusMessage winner={winner} current={current} />
-      <Board board={current.board} handelSquareClick={handelSquareClick} />
+      <Board
+        board={current.board}
+        handelSquareClick={handelSquareClick}
+        winningSquares={winningSquares}
+      />
+      <button type="button" onClick={onNewGame}>
+        Start new game
+      </button>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
